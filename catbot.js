6c1d32008bd321119e2b8a7642d0bf7a58a16c6b
Eric Wyler <ewyler@gmail.com>
2016-10-05 23:56:33 -0400

    aiur

--- catbot.js
+++ catbot.js
@@ -97,6 +97,22 @@ controller.hears(
 );
 
 controller.hears(
+    ['what do you think of rick'],
+    'direct_message,direct_mention,mention',
+    async (bot, message) => {
+        bot.reply(message, ":parrot: definitely a butt :parrot:")
+    }
+);
+
+controller.hears(
+    ['will do it'],
+    'direct_message,direct_mention,mention',
+    async (bot, message) => {
+        bot.reply(message, "My life for aiur.")
+    }
+);
+
+controller.hears(
     ['.*'],
     'direct_message,direct_mention,mention',
     (bot, message) => {

77a7b85c0e6ca22fb104f9fde87649151c383f2a
Eric Wyler <ewyler@gmail.com>
2016-12-27 17:36:20 -0500

    every 15s

--- catbot.js
+++ catbot.js
@@ -81,7 +81,7 @@ schedule.scheduleJob(EACH_DAY_AT_MIDNIGHT, function() {
     coffeeManager.reset();
 });
 
-const EVERY_15_SECONDS = '*/15 * * * *';
+const EVERY_15_SECONDS = '*/15 * * * * *';
 const REPOS = ['hn-core', 'hn-webpack', 'hn-nerd-experience', 'hn-enterprise-portal', 'hn-marketing-sales'];
 
 schedule.scheduleJob(EVERY_15_SECONDS, function() {
